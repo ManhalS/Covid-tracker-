c508faa9c7b87b02374a9d16c0cdc0bf6b4807ed
manhals <mn-angel@hotmail.com>
2021-04-28 11:09:05 -0700

    update

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Gstatus from './compnents/Gstatus'
+import Searchbar from './compnents/searchbr'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <Searchbar/>
+     <Gstatus/>
     </div>
   );
 }
